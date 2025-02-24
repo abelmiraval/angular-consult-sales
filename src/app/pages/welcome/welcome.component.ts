ccaff7b828fafb5821801a6c24eb6c60d06258f0
abelmiraval <abel.miraval.rp@viaexperis2.pe>
2025-02-24 11:01:49 -0500

    tools: Init project

---
+++ src/app/pages/welcome/welcome.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-welcome',
+  templateUrl: './welcome.component.html',
+  imports: [
+  ],
+  styleUrl: './welcome.component.less'
+})
+export class WelcomeComponent {
+  constructor() {}
+}
